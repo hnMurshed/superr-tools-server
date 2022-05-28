672f6eee984048f91d22f634d0c37df6ac06a4f5
Murshedul <murshedbinamir@gmail.com>
2022-05-29 00:09:15 +0600

    router link changed on

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 
 // use middleware
 
-app.use(cors({origin: 'https://superr-tools.web.app'}))
+// app.use(cors({origin: 'https://superr-tools.web.app'}));
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfl5mwm.mongodb.net/?retryWrites=true&w=majority`;
@@ -117,7 +118,7 @@ const run = async () => {
         // get order based on user
         app.get('/orders', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const query = {user: email};
+            const query = {email: email};
             const orders = await orderCollection.find(query).toArray();
             res.send(orders);
         });
