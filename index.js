0cd2e97452b15f39a31b88e0fbff98a8c073999b
Murshedul <murshedbinamir@gmail.com>
2022-05-28 23:20:14 +0600

    product inserted

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // use middleware
-app.use(cors());
+const corsConfig = {
+    origin: 'https://superr-tools.web.app/',
+    credentials: true,
+    };
+app.use(cors(corsConfig))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gfl5mwm.mongodb.net/?retryWrites=true&w=majority`;
@@ -22,12 +26,49 @@ const run = async () => {
         const userCollection = client.db('SuperrTools').collection('users');
         const orderCollection = client.db('SuperrTools').collection('orders');
 
+        // verify access token
+        const verifyToken = (req, res, next) => {
+            const authHeader = req.headers.authorization;
+            if (!authHeader) {
+                return res.status(401).send({ message: 'Unauthorized' });
+              };
+        
+            const acceesToken = authHeader.split(' ')[1];
+            jwt.verify(acceesToken, process.env.SECRET_ACCESS_TOKEN, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({ message: 'Fobidden access' });
+            };
+            req.decoded = decoded;
+    
+            next();
+            });
+        };
+
+        // check the requester if admin or not
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.user;
+            const requesterAcount = await userCollection.findOne({ user: requester });
+      
+            if (requesterAcount.role === 'admin') {
+              next();
+            }
+            else {
+              res.status(403).send({ message: 'Fobidden' });
+            }
+          };
+
         app.get('/tools', async (req, res) => {
             const pageNo = parseInt(req.query.page);
             const productsQuantityToShow = parseInt(req.query.quantity);
             const tools = await productCollection.find().skip(pageNo*productCollection).limit(productsQuantityToShow).toArray();
 
             res.send(tools);
+        });
+
+        app.post('/addproduct', async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
         });
 
         app.get('/tool/:id', async (req, res) => {
@@ -74,7 +115,22 @@ const run = async () => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
             res.send(result);
-        })
+        });
+
+        // get order based on user
+        app.get('/orders', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const query = {user: email};
+            const orders = await orderCollection.find(query).toArray();
+            res.send(orders);
+        });
+
+        app.get('/checkadmin', async (req, res) => {
+            const email = req.headers.email;
+            const user = await userCollection.findOne({ user: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+          });
 
         app.get('/check-mongo-connection', (req, res) => {
             res.send('MongoDB connected successfully, YAY!');
