c1f51fa79e9aad7e0d0ebfacfa0eb9e813dd08af
Murshedul <murshedbinamir@gmail.com>
2022-05-28 20:06:53 +0600

    user orders info added and updated stock after order

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 
 const app = express();
@@ -20,6 +20,7 @@ const run = async () => {
         await client.connect();
         const productCollection = client.db('SuperrTools').collection('products');
         const userCollection = client.db('SuperrTools').collection('users');
+        const orderCollection = client.db('SuperrTools').collection('orders');
 
         app.get('/tools', async (req, res) => {
             const pageNo = parseInt(req.query.page);
@@ -28,6 +29,29 @@ const run = async () => {
 
             res.send(tools);
         });
+
+        app.get('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await productCollection.findOne(query);
+            res.send(tool);
+        });
+
+        // updating item stock
+        app.put('/tool/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedStock = req.query.updatedStock;
+            console.log(id, updatedStock);
+            const filter = {_id: ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    stock: updatedStock
+                }
+            }
+            const result = await productCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+        })
 
         app.put('/user', async (req, res) => {
             const email = req.body.email;
@@ -43,6 +67,12 @@ const run = async () => {
             const accessToken = jwt.sign({user: email}, process.env.SECRET_ACCESS_TOKEN, {expiresIn: '1day'});
             result.accessToken = accessToken;
 
+            res.send(result);
+        });
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
             res.send(result);
         })
 
